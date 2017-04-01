501326d155d926c68215819e8010ae3f9089e7d6
samouel <samouel@acer>
2017-04-01 19:39:42 +0430

    samouel project fallow jadi

--- webb/admin.py
+++ webb/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Expense, Income
 
 # Register your models here.
+admin.site.register(Expense)
+admin.site.register(Income)
--- webb/models.py
+++ webb/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Expense(models.Model):
+	text = models.CharField(max_length = 256)
+	date = models.DateTimeField()
+	amount = models.BigIntegerField()
+	user = models.ForeignKey(User)
+	def __unicode__(text):
+		return "{}-{}".format(self.date, self.amount)
+
+class Income(models.Model):
+	text = models.CharField(max_length = 256)
+	date = models.DateTimeField()
+	amount = models.BigIntegerField()
+	user = models.ForeignKey(User)
+	def __unicode__(text):
+		return "{}-{}".format(self.date, self.amount)
+
+	
